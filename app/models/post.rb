0f7c99a1cfe4d0789ad3ae04a63bce2208d5146e
Sungoh Park <spark449062@gmail.com>
2021-09-27 00:05:34 -0600

    commit tree 0d87365e3b28b7094038178273fd824efb3af83d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :title_custom_validation
+
+    def title_custom_validation
+        unless (title.include? "Won't Believe") || (title.include? "Secret") || (title.include? "Top [/\d/]") || (title.include? "Guess")
+            errors.add!(:title, "title needs to include clickbait expression")
+        end
+    end
+
 end
